0873891dbdb90b0632d9284173a5301e6af77952
Kyle Geske <stungeye@gmail.com>
2017-10-22 20:33:10 +0000

    Commenting the students controller. Removed the magic from the index view link to student.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,9 +1,13 @@
 class StudentsController < ApplicationController
+  # GET /students
+  # GET /
   def index
     @students = Student.order(:name)
   end
+  # Load app/views/students/index.html.erb
 
   def show
     @student = Student.find(params[:id])
   end
+  # Load app/views/students/show.html.erb
 end
