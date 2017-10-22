2eaaa7821b30ee563d21b5273885df4c2024632e
Kyle Geske <stungeye@gmail.com>
2017-10-22 21:18:00 +0000

    Prep work for create/new actions and views for student.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,4 +10,14 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
   # Load app/views/students/show.html.erb
+
+  # GET /students/new
+  def new
+  end
+  # Load app/views/students/new.html.erb
+
+  # POST /students
+  def create
+  end
+  # No view, action will redirect or render.
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  # Show Collection
+
+  # Show Collection (index READ)
   get 'students', to: 'students#index', as: 'students'
 
-  # Show Member
+  # Show Member (show READ)
   get 'students/:id', to: 'students#show',  as: 'student', id: /\d+/
+
+  # Display New Student Form (new CREATE)
+  get 'students/new', to: 'students#new', as: 'new_student'
+
+  # Process Posted New Student Form (create CREATE)
+  post 'students', to: 'students#create'
 
   # Root routes
   root to: 'students#index'
